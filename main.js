4cdc5e56b3e4164421030bfa8c4b98ca29129cb7
star-coder-sammu <101115549+star-coder-sammu@users.noreply.github.com>
2022-12-21 15:33:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,88 @@
+song = "";
+song2 = "";
+song3 = "";
+leftWristX= 0;
+leftWristY= 0;
+rightWristX= 0;
+rightWristY= 0;
+
+function preload()
+{
+    song = loadSound("inten.mp3");
+	song2 = loadSound("music.mp3");
+	song3 = loadSound("wrap.mp3");
+}
+
+function setup() {
+	canvas =  createCanvas(600, 500);
+	canvas.center();
+
+	video = createCapture(VIDEO);
+	video.hide();
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
+
+function modelLoaded()
+{
+    console.log('posenet is intialised');
+}
+
+function gotPoses (results)
+{
+    if(results.lenght > 0)
+    {
+        console.log(result);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist =" + scoreLeftWrist);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = "+ leftWristX +"leftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = "+ rightWristX +"rightWristY = " + rightWristY);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
